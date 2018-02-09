40e2f3dcb81d4a4c6193ee81be57b8fc3daaf44d
AraceliGS <aragutarras@gmail.com>
2018-02-09 18:46:12 -0500

    añadiendo avances

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   let currentStep = 0;
   $('#step1').hide();
   $('#step2').hide();
-
+  
   $('#btnStep1').click(function() {
     $('#step1').show();
     $('#step2').hide();
