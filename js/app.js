ceee72f34cc96a90a14799a7641286bc217fbd55
AraceliGS <aragutarras@gmail.com>
2018-02-09 15:50:58 -0500

    añadiendo modelo base

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+  let currentStep = 0;
+  $('#step1').hide();
+  $('#step2').hide();
+
+  $('#btnStep1').click(function() {
+    $('#step1').show();
+    $('#step2').hide();
+    currentStep = 1;
+    console.log(currentStep);
+  });
+
+  $('#btnStep2').click(function() {
+    $('#step1').hide();
+    $('#step2').show();
+    currentStep = 2;
+    console.log(currentStep);
+  });
+});
 
